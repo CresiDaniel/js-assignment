b4d7b5f64635e2dc9c297263a187bdb6d7c7f3b3
Cresi <68217365+CresiDaniel@users.noreply.github.com>
2020-07-15 22:24:02 +0530

    Day4 Assignment

---
+++ Day4Assignment/f1.js
@@ -0,0 +1,4 @@
+console.log("hello");
+let rating=prompt("whats ur rating fro our website out of 5?");
+console.log(rating);
+alert("Thank u for your rating.");
---
+++ Day4Assignment/t1.js
@@ -0,0 +1,10 @@
+console.log("hello");
+var age=prompt("enter ur age");
+if(age<21)
+{
+    console.log("Cannot drink");
+
+}
+else{
+    console.log("Can drink");
+}
